4d6e213835af0a90e24eac30c007367435e826d2
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2018-06-09 11:06:13 -0500

    Preparing for release 0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='design-explorer',
-      version='0.1',
+      version='0.2',
       description='Design Explorer',
       long_description=readme(),
       classifiers=[
