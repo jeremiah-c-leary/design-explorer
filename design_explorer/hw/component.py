4d9045d54ca4e52fe9da451e8a1dce393c310470
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2018-11-20 09:27:13 -0600

    Adding hw.component class and tests.

---
+++ design_explorer/hw/component.py
@@ -0,0 +1,29 @@
+
+from design_explorer import utils
+
+
+class create():
+    '''
+    Creates an object with a given instance name.
+    '''
+
+    def __init__(self, name):
+        self.name = name
+#        self.interfaces = None
+#        self.interface_types = None
+
+#    def _add_interface(self, oInterface, sInterfaceType):
+#        self.interfaces = utils.append_to_list(self.interfaces, oInterface)
+#        self.interface_types = utils.append_to_list(self.interface_types, sInterfaceType)
+#
+#    def add_source_interface(self, oInterface):
+#        self._add_interface(oInterface, 'Source')
+#
+#    def add_sink_interface(self, oInterface):
+#        self._add_interface(oInterface, 'Sink')
+#
+#    def _extract_interface(self, oInterface, sInterfaceType):
+#        for sLine in oInterface.extract_port_list(sInterfaceType):
+#            return '    ' + sLine
+#
+#        return lReturn
