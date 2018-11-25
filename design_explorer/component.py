a0f0a05ca9e37fff6509735d0b8cc7b9f53ae5a9
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2018-11-25 09:33:31 -0600

    Updates for issue #8
      1)  Interfaces are now deepcopied
      2)  Added ability to rename interfaces as they are added

--- design_explorer/component.py
+++ design_explorer/component.py
@@ -1,4 +1,5 @@
 
+import copy
 from design_explorer import utils
 
 
@@ -12,15 +13,17 @@ class create():
         self.interfaces = None
         self.interface_types = None
 
-    def _add_interface(self, oInterface, sInterfaceType):
+    def _add_interface(self, oInterface, sInterfaceType, sInterfaceName=None):
+        if sInterfaceName:
+            oInterface.name = sInterfaceName
         self.interfaces = utils.append_to_list(self.interfaces, oInterface)
         self.interface_types = utils.append_to_list(self.interface_types, sInterfaceType)
 
-    def add_source_interface(self, oInterface):
-        self._add_interface(oInterface, 'Source')
+    def add_source_interface(self, oInterface, sInterfaceName=None):
+        self._add_interface(copy.deepcopy(oInterface), 'Source', sInterfaceName)
 
-    def add_sink_interface(self, oInterface):
-        self._add_interface(oInterface, 'Sink')
+    def add_sink_interface(self, oInterface, sInterfaceName=None):
+        self._add_interface(copy.deepcopy(oInterface), 'Sink', sInterfaceName)
 
     def get_interface(self, sInterfaceName):
         for oInterface in self.interfaces:
--- design_explorer/tests/component/test_component.py
+++ design_explorer/tests/component/test_component.py
@@ -35,6 +35,25 @@ class test_hw_component(unittest.TestCase):
         self.assertEqual(oComponent.interfaces[1].ports[0].name, 'Port3')
         self.assertEqual(oComponent.interfaces[1].ports[1].name, 'Port4')
 
+    def test_component_add_sink_and_source_interface_w_renaming_methods(self):
+        oPort1 = port.create('Port1')
+        oPort2 = port.create('Port2')
+        oPort3 = port.create('Port3')
+        oPort4 = port.create('Port4')
+        oIntSource = interface.create('Interface1')
+        oIntSource.add_source_port(oPort1)
+        oIntSource.add_sink_port(oPort2)
+        oIntSink = interface.create('Interface2')
+        oIntSink.add_source_port(oPort3)
+        oIntSink.add_sink_port(oPort4)
+        oComponent = component.create('component')
+        oComponent.add_source_interface(oIntSource, 'source_interface')
+        oComponent.add_sink_interface(oIntSink, 'sink_interface')
+        self.assertEqual(oComponent.interfaces[0].name, 'source_interface')
+        self.assertEqual(oComponent.interfaces[1].name, 'sink_interface')
+        self.assertEqual(oIntSource.name, 'Interface1')
+        self.assertEqual(oIntSink.name, 'Interface2')
+
     def test_component_get_interface_method(self):
         oIntSource = interface.create('Interface1')
         oIntSink = interface.create('Interface2')
