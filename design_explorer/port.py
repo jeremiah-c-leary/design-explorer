3590c26b5a752657f23fd5f3abd7c0021f7e873d
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2019-04-25 19:32:22 -0500

    Adding value checking on port directions.

--- design_explorer/port.py
+++ design_explorer/port.py
@@ -7,5 +7,10 @@ class create():
     def __init__(self, name, width=None, direction='in', description=None):
         self.name = name
         self.width = width
-        self.direction = direction
+        self.direction = self._validate_direction(direction)
         self.description = description
+
+    def _validate_direction(self, sString):
+        if sString not in ['in', 'out', 'inout']:
+            raise ValueError('Only in, out, or inout are valid port directions.')
+        return sString
