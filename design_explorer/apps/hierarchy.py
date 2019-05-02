98171c68cde0282c3b41a62679462438259c19c2
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2019-05-01 21:52:43 -0500

    Adding hierarchy.extract_connections function which extracts all connections in a given hierarchy.

--- design_explorer/apps/hierarchy.py
+++ design_explorer/apps/hierarchy.py
@@ -51,13 +51,18 @@ def extract_mid_points(lHierarchy):
 def extract_connections(oSystem, lMidPoints):
     lReturn = []
     for sMidPoint in lMidPoints:
-        print sMidPoint
-        try:
+        if sMidPoint.count('.') == 0:
+            if oSystem.connections is not None:
+                lReturn.extend(oSystem.connections)
+        else:
+            sMidPoint = remove_system_name(sMidPoint)
             oComponent = oSystem.get_component_named(sMidPoint)
-            print oComponent
-            print oComponent.name
-            lReturn.extend(oComponent.connections)
-        except ValueError:
-            print 'Could not find component ' + sMidPoint
+            if oComponent.connections is not None:
+                lReturn.extend(oComponent.connections)
 
     return lReturn
+
+
+def remove_system_name(sString):
+    iIndex = sString.find('.')
+    return sString[iIndex + 1:]
---
+++ design_explorer/tests/apps/hierarchy/test_extract_connections.py
@@ -0,0 +1,75 @@
+
+
+import unittest
+import design_explorer as de
+
+class test_connections(unittest.TestCase):
+
+
+    def setUp(self):
+
+        self.oSystem = de.system.create('Top Level')
+
+        oCca1 = self.oSystem.add_component(de.hw.cca.create('Cca1'))
+
+        oCca1Comp1 = oCca1.add_component(de.component.create('Comp1', 'Comp1'))
+        oCca1Comp1.create_interface('I1')
+        oCca1Comp2 = oCca1.add_component(de.component.create('Comp2', 'Comp2'))
+        oCca1Comp2.create_interface('I1')
+        oCca1Comp3 = oCca1.add_component(de.component.create('Comp3', 'Comp3'))
+        oCca1Comp3.create_interface('I1')
+
+        oCca2 = self.oSystem.add_component(de.hw.cca.create('Cca2'))
+        oCca2Comp1 = oCca2.add_component(de.component.create('Comp1', 'Comp1'))
+        oCca2Comp1.create_interface('I1')
+        oCca2Comp2 = oCca2.add_component(de.component.create('Comp2', 'Comp2'))
+        oCca2Comp2.create_interface('I1')
+        oCca2Comp3 = oCca2.add_component(de.component.create('Comp3', 'Comp3'))
+        oCca2Comp3.create_interface('I1')
+
+        oCca3 = self.oSystem.add_component(de.hw.cca.create('Cca3'))
+        oCca3Comp1 = oCca3.add_component(de.component.create('Comp1', 'Comp1'))
+        oCca3Comp1.create_interface('I1')
+        oCca3Comp2 = oCca3.add_component(de.component.create('Comp2', 'Comp2'))
+        oCca3Comp2.create_interface('I1')
+        oCca3Comp3 = oCca3.add_component(de.component.create('Comp3', 'Comp3'))
+        oCca3Comp3.create_interface('I1')
+
+        oCca4 = self.oSystem.add_component(de.hw.cca.create('Cca4'))
+        oCca4Comp1 = oCca4.add_component(de.component.create('Comp1', 'Comp1'))
+        oCca4Comp1.create_interface('I1')
+        oCca4Comp2 = oCca4.add_component(de.component.create('Comp2', 'Comp2'))
+        oCca4Comp2.create_interface('I1')
+        oCca4Comp3 = oCca4.add_component(de.component.create('Comp3', 'Comp3'))
+        oCca4Comp3.create_interface('I1')
+
+        self.oCon1 = de.connection.create('con1', oCca1, 'Cca1.Comp1.I1', 'Cca1.Comp2.I1', False)
+        self.oCon2 = de.connection.create('con2', oCca2, 'Cca2.Comp1.I1', 'Cca2.Comp2.I1', False)
+        self.oCon3 = de.connection.create('con3', oCca3, 'Cca3.Comp1.I1', 'Cca3.Comp2.I1', False)
+        self.oCon4 = de.connection.create('con4', oCca3, 'Cca4.Comp1.I1', 'Cca4.Comp2.I1', False)
+
+        oCca1.add_connection(self.oCon1)
+        oCca2.add_connection(self.oCon2)
+        oCca3.add_connection(self.oCon3)
+        oCca4.add_connection(self.oCon4)
+
+
+    def test_multiple_cca_extract(self):
+
+        lHierarchy = de.apps.hierarchy.extract(self.oSystem)
+
+        lMidPoints = de.apps.hierarchy.extract_mid_points(lHierarchy)
+
+        lActual = de.apps.hierarchy.extract_connections(self.oSystem, lMidPoints)
+       
+        lExpected = []
+        lExpected.append(self.oCon1)
+        lExpected.append(self.oCon2)
+        lExpected.append(self.oCon3)
+        lExpected.append(self.oCon4)
+ 
+        self.assertEqual(lExpected, lActual)
+
+
+if __name__ == '__main__':
+    unittest.main()
